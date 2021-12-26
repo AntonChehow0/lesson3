0ee31caa97811530044c293a006277a9aab2727f
Мусонов Виталий <musonov.vitaliy@osinit.com>
2021-12-26 21:03:45 +0300

    Created project

---
+++ lesson3/lesson3/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  lesson3
+//
+//  Created by Виталий  on 26.12.2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lesson3/lesson3/lesson3App.swift
@@ -0,0 +1,18 @@
+//
+//  lesson3App.swift
+//  lesson3
+//
+//  Created by Виталий  on 26.12.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct lesson3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
